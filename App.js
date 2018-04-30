f28239dae6b968807ce6c7aebb958e3611cf6973
Lydia Law <lydialawli@gmail.com>
2018-04-30 17:21:51 +0200

    adding button and stack navigator

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { StackNavigator } from 'react-navigation'
 
-export default class App extends React.Component {
+
+class HomeScreen extends React.Component {
+  _handleButtonPress = () => {
+    Alert.alert(
+      'Button pressed!',
+      'Camera should pop-up here',
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hakuna matata</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Take a pic"
+          onPress={this._handleButtonPress}
+        />
       </View>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+)
+
+export default class App extends React.Component{
+  render() {
+    return <RootStack />
   }
 }
 
