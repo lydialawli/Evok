8ea804944308e9c2e700454925fe8d360c4f7cc0
Lydia Law <lydialawli@gmail.com>
2018-12-09 14:38:17 +0100

    created move, savetext, get text function

---
+++ fileSystem.js
@@ -0,0 +1,35 @@
+import { FileSystem } from 'expo'
+
+//this file is for general functions not specific to Evok functionality
+
+const fileSystem = {}
+
+fileSystem.move = (originalFile, currentFolder, fileName, callback) => {
+    FileSystem.moveAsync({ from: originalFile, to: currentFolder + '/' + fileName })
+        .then(callback)
+        .catch(err => console.error(err))
+}
+
+
+fileSystem.saveText = (text, to, callback) => {
+    FileSystem.writeAsStringAsync(to, text)
+        .then(callback)
+        .catch(err => console.error(err))
+}
+
+
+
+fileSystem.getText() = (from, callback) => {
+    FileSystem.readDirectoryAsync(from)
+        .then((result) => {
+            callback(result) 
+        })
+        .catch(err => console.error(err))
+{
+
+
+
+
+
+
+export default fileSystem
--- globalFilesystem.js
+++
@@ -1,5 +0,0 @@
-import { FileSystem } from 'expo'
-
-//this file is to store general functions not specific to Evok functions
-
-const globalFileSystem = {}
